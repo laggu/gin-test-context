0d725e18132d5bf6747e4e88baafb4d5914c038f
laggu91 <laggu91@gmail.com>
2024-10-04 20:10:22 +0900

    test: body test

---
+++ body_test.go
@@ -0,0 +1,70 @@
+package GinTestContext
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-faker/faker/v4"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_body_SetBody(t *testing.T) {
+	b := struct {
+		A string
+		B int
+		C float64
+	}{}
+	err := faker.FakeData(&b)
+	require.NoError(t, err)
+
+	mock := body{}
+	mock.SetBody(b)
+
+	require.Equal(t, b, mock.Body)
+}
+
+func Test_body_writeBodyToContext(t *testing.T) {
+	testCases := []struct {
+		name           string
+		body           interface{}
+		expectedResult error
+	}{
+		{
+			name: "success",
+			body: struct {
+				A string
+				B int
+				C float64
+			}{
+				A: "a",
+				B: 1,
+				C: 1.23,
+			},
+			expectedResult: nil,
+		},
+		{
+			name:           "empty_body",
+			body:           nil,
+			expectedResult: nil,
+		},
+		{
+			name:           "json_parsing_error",
+			body:           make(chan int),
+			expectedResult: ErrUnsupportedBodyType,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			mock := body{}
+			mock.SetBody(tc.body)
+
+			c, _ := gin.CreateTestContext(httptest.NewRecorder())
+			c.Request = &http.Request{}
+
+			err := mock.writeBodyToContext(c)
+			require.Equal(t, tc.expectedResult, err)
+		})
+	}
+}
